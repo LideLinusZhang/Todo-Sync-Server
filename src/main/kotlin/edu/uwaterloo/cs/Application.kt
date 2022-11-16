963eeab43c941c3d15fc8313dac7c5040933d6ca
Linus Zhang <l625zhan@uwaterloo.ca>
2022-11-16 17:45:41 -0500

    Cleanup

--- src/main/kotlin/edu/uwaterloo/cs/Application.kt
+++ src/main/kotlin/edu/uwaterloo/cs/Application.kt
@@ -1,8 +1,10 @@
 package edu.uwaterloo.cs
 
+import edu.uwaterloo.cs.plugins.configureRouting
+import edu.uwaterloo.cs.plugins.configureSecurity
+import edu.uwaterloo.cs.plugins.configureSerialization
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
-import edu.uwaterloo.cs.plugins.*
 
 fun main() {
     embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
